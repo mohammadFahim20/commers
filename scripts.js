dfd8130ae67656bde80cc5363e549f497405d340
Mohammad-Fahim <136916632+mohammadFahim20@users.noreply.github.com>
2023-08-15 01:00:45 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const menuIcon=document.getElementById("menu-icon")
+const menu=document.getElementById("menu")
+
+menuIcon.addEventListener("click",()=>{
+    if(menu.className==="hidden"){
+        menu.classList.remove("hidden")
+    }
+    else(menu.className==="hidden")
+    {
+        menu.classList.add("hidden")
+    }
+
+})
